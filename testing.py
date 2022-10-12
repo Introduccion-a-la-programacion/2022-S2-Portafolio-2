68ff975a0139c8cc5b72051fe76d6543d6cd47ca
Cristian Campos Agüero <ccampos04@gmail.com>
2022-10-12 14:25:46 -0600

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,87 +1,43 @@
-import Portafolio3;
+import Portafolio2;
 import pytest;
 
 
-def test_invertirNumero_1():
-    assert Portafolio3.invertirNumero(24) == 42
+def test_susbistas_impares_1():
+    assert Portafolio2.susbistas_impares([23, 6756, 5533, 811]) == [[2,3], [5,5,3,3], [8, 1, 1]]
     
-def test_invertirNumero_2():
-    assert Portafolio3.invertirNumero(-123) == -321
-    
-def test_invertirNumero_3():
-    assert Portafolio3.invertirNumero(120) == 21
+def test_susbistas_impares_2():
+    assert Portafolio2.susbistas_impares ([152, 45, 60]) == [[4,5]]
+
+def test_susbistas_impares_3():
+    assert isinstance(str(Portafolio2.susbistas_impares ([])), str) == isinstance("Error en la entrada, no se permite lista vacía", str)
 ###########################################################################
+
+def test_es_computable_1():
+    assert Portafolio2.es_computable(332) == True
     
-def test_divisores_1():
-    assert Portafolio3.divisores(24) == [24, 12, 8, 6, 4, 3, 2, 1]
+def test_es_computable_2():
+    assert Portafolio2.es_computable(6877) == True
+    
+def test_es_computable_3():
+    assert Portafolio2.es_computable(33) == False
       
 ###########################################################################
 
-def test_multiplicacion_1():
-    assert Portafolio3.multiplicacion(5, 2) == 10
+matriz = [ [9, 2, 0, 3, 4], [3, 0, 0, 0, 9], [0, 0, 0, 0, 0], [4, 0, 0, 0, 3], [1, 3, 0, 9, 1] ]
+
+def test_matriz_diamante_1():
+    assert Portafolio2.matriz_diamante(matriz) == True
+
     
 ###########################################################################    
 
-def test_division_1():
-    assert Portafolio3.division(25, 5) == 5
+def test_max_profundidad_lista_1():
+    assert Portafolio2.max_profundidad_lista([[["H"], [["M"],[[12]]], [13]], [[45]], []]) == [[["H"], [["M"], [[12]]], [13]], [["M"], [[12]]], [[12]], [12]]
     
-def test_division_2():
-    assert isinstance(str(Portafolio3.division(25, 0)), str) == isinstance('Error: División entre cero', str)
-    
-###########################################################################    
+def test_max_profundidad_lista_2():
+    assert Portafolio2.max_profundidad_lista([[["Hola"]], "Mundo"]) == [[["Hola"]], ["Hola"]]
 
-def test_corrimientoAEntero_1():
-    assert Portafolio3.corrimientoAEntero(133.5) == 1335
-    
-def test_corrimientoAEntero_2():
-    assert Portafolio3.corrimientoAEntero(-133.5) == -1335    
-    
-###########################################################################    
-
-def test_contarDigitosFlotante_1():
-    assert Portafolio3.contarDigitosFlotante(133.578) == 6
-    
-def test_contarDigitosFlotante_2():
-    assert Portafolio3.contarDigitosFlotante(-133.578) == 6    
-    
-###########################################################################    
-
-def test_indiceNumero_1():
-    assert Portafolio3.indiceNumero(1335, 3) == 5
-    
-def test_indiceNumero_2():
-    assert isinstance(str(Portafolio3.indiceNumero(1335, 8)), str) == isinstance('Error: Indice fuera del rango del número', str)
-    
-###########################################################################    
-
-def test_cortarNumero_1():
-    assert Portafolio3.cortarNumero(1335, 1, 2) == 33
-
-def test_cortarNumero_2():
-    assert isinstance(str(Portafolio3.cortarNumero(1335, 8, 2)), str) == isinstance('Error: Indices fuera del rango del número', str)  
-
-###########################################################################    
-
-def test_textoPalindromo_1():
-    assert Portafolio3.textoPalindromo("OSO") == True
-    
-def test_textoPalindromo_2():
-    assert Portafolio3.textoPalindromo("CASA") == False
-    
-########################################################################### 
-
-def test_multiplicarElmentosLista_1():
-    assert Portafolio3.multiplicarElementosLista([2, 8, 5, 10]) == 160
-    
-def test_multiplicarElmentosLista_2():
-    assert Portafolio3.multiplicarElementosLista([2, 3, 5, 10.5, 18.3]) == 2
-    
-def test_multiplicarElmentosLista_3():
-    assert Portafolio3.multiplicarElementosLista([2, 8, 5, 10, 0]) == 0
-    
-def test_multiplicarElmentosLista_4():
-    assert Portafolio3.multiplicarElementosLista([2, 8, True, [], "Hola", 5]) == 16    
+def test_max_profundidad_lista_3():
+    assert Portafolio2.max_profundidad_lista([["Distanciamiento"], ["Social"]]) == [["Distanciamiento"]]
 
 
-    
-    
